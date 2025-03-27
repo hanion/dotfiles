1394bef2545226f7d8b3d92eeb9b5deccd8c7d8b
hanion <55713249+hanion@users.noreply.github.com>
2025-03-27 23:49:55 +0300

    nvim remove neotree

--- nvim/init.lua
+++ nvim/init.lua
@@ -954,17 +954,6 @@ require("lazy").setup({
 		vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git Commit" })
 	},
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-		vim.keymap.set("n", "<leader>ft", "<cmd>Neotree toggle<CR>", { desc = "neotree toggle" }),
-	},
-	{
 		"nvim-neotest/nvim-nio"
 	},
 	{
